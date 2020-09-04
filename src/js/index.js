69edb8a66e86360b145818f8c8500fc916955a2c
Davey Wright <davey.w.wright@gmail.com>
2020-09-04 11:28:16 +0100

    add scripts from webflow

---
+++ src/js/index.js
@@ -0,0 +1,39 @@
+window.PursuitCollective = {};
+
+$('.subscription-article-topic').each(function() {
+  var text = $(this).text();
+
+  var DOMElement = '<label class="w-checkbox checkbox-field"> ' +
+    '<input type="checkbox" id="'+ text +'" name="'+ text.replace(/' '/g, '-') +'" data-name="'+ text +'" class="w-checkbox-input checkbox">' +
+      '<span for="'+ text +'" class="checkbox-label w-form-label">'+ text +'</span>' +
+  '</label>';
+
+  $('.subscription-article-checkboxes').append(DOMElement);
+});
+
+
+
+var openSearchBar = function(){
+  $(this).addClass('.navbar_search-button--active');
+  $('.subnav-wrapper').addClass('subnav-wrapper--hidden');
+  $('.navbar_logo').addClass('navbar_logo--hidden');
+  
+  window.setTimeout(function(){
+    $('.navbar_search-wrapper').removeClass('navbar_search-wrapper--hidden');
+  }, 350);
+};
+
+var closeSearchBar = function(){
+  $('.navbar_search-wrapper').addClass('navbar_search-wrapper--hidden');
+  
+  window.setTimeout(function(){
+    $('.subnav-wrapper').removeClass('subnav-wrapper--hidden');
+    $('.navbar_logo').removeClass('navbar_logo--hidden');
+  }, 350);
+};
+
+
+
+$('.navbar_search-button').click(openSearchBar);
+$('.footer_search-link').click(openSearchBar);
+$('.navbar_search-cancel-btn').click(closeSearchBar);   
