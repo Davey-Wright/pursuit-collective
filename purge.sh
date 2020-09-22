c093e752b83c3dc128998cd52eda757256635689
Davey Wright <davey.w.wright@gmail.com>
2020-09-22 21:31:19 +0100

    shaka

--- purge.sh
+++ purge.sh
@@ -7,6 +7,6 @@ http://purge.jsdelivr.net/ \
 -H 'content-type: application/json' \
 -d '{ 
   "path": [
-    "gh/Davey-Wright/pursuit-collective@latest/dist/application.min.js"
+    "gh/Davey-Wright/pursuit-collective@latest/dist/application.min.css"
   ]
 }'
